d3f3f208a71631157abd2e27c242af6259a43e47
houzairkoussa <houzairmk@icloud.com>
2021-12-13 10:09:13 +0400

    added answer file and tests

---
+++ exercises/consecutive-string-count/answer.js
@@ -0,0 +1,3 @@
+const modify = (text) => {
+
+}
---
+++ exercises/consecutive-string-count/exercise.js
@@ -0,0 +1,10 @@
+const modify = (text) => {
+  // TODO
+}
+
+// Tests -
+// * You can run these tests using the terminal command `node exercise`
+console.log(modify("aaabbbaacca"));
+console.log(modify("abcd"));
+console.log(modify(""));
+console.log(modify("bbbbbbaaaa"));
