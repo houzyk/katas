96f89a782651c5484e13bc1b0ac2e08145b0f4de
houzairkoussa <houzairmk@icloud.com>
2021-12-13 13:21:36 +0400

    answer added

--- exercises/consecutive-string-count/answer.js
+++ exercises/consecutive-string-count/answer.js
@@ -1,3 +1,12 @@
 const modify = (text) => {
-
+  const result = [];
+  let counter = [];
+  text.split('').forEach((char, charIndex, charArray) => {
+    counter.push(char);
+    if (char !== charArray[charIndex + 1]) {
+      result.push(`${counter.filter(c => c === char).length}${char}`);
+      counter = [];
+    }
+  });
+  return result.join('');
 }
--- exercises/consecutive-string-count/exercise.js
+++ exercises/consecutive-string-count/exercise.js
@@ -4,7 +4,8 @@ const modify = (text) => {
 
 // Tests -
 // * You can run these tests using the terminal command `node exercise`
-console.log(modify("aaabbbaacca"));
-console.log(modify("abcd"));
+console.log(modify("aaabbbaaccaz"));
+console.log(modify("abcdz"));
 console.log(modify(""));
-console.log(modify("bbbbbbaaaa"));
+console.log(modify("bbbbbbaaaaz"));
+console.log(modify("zzzaazzzaaazzzzzzzzzza"));
